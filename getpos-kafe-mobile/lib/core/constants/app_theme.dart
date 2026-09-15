91aa64a891bc3cb0007d1d7454c10e1a7cb48b43
TemurBekibayev <temurkhann2000@gmail.com>
2026-09-15 17:07:24 +0500

    Fix Java 17 and Android build dependencies

--- getpos-kafe-mobile/lib/core/constants/app_theme.dart
+++ getpos-kafe-mobile/lib/core/constants/app_theme.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'app_colors.dart';
 
 class AppTheme {
@@ -13,7 +12,6 @@ class AppTheme {
         surface: AppColors.surface,
         error: AppColors.error,
       ),
-      textTheme: GoogleFonts.interTextTheme(),
       appBarTheme: const AppBarTheme(
         backgroundColor: Colors.white,
         foregroundColor: AppColors.textPrimary,
